2554b5a1b03947a7a0839cdb95f115104fa12162
omp10 <omparteki2002@gmail.com>
2024-08-11 16:07:55 +0530

    Corrected path

--- Backend/server.js
+++ Backend/server.js
@@ -26,12 +26,12 @@ app.use("/api/v1/tv", protectRoute, tvRoutes);
 app.use("/api/v1/search", protectRoute, searchRoutes);
 
 if(ENV_VARS.NODE_ENV==="production"){
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "/Frontend/dist")));
 
 }
 
 app.get("*",(req,res)=>{
-  res.sendFile(path.resolve(__dirname, "frontend","dist","index.html"));
+  res.sendFile(path.resolve(__dirname, "Frontend","dist","index.html"));
 });
 
 app.listen(PORT, () => {
