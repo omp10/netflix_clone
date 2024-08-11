776fba32641b13662f7b914ab5959ad54d037d23
omp10 <omparteki2002@gmail.com>
2024-08-11 17:33:52 +0530

    error fixed

--- Frontend/src/pages/home/AuthScreen.jsx
+++ Frontend/src/pages/home/AuthScreen.jsx
@@ -12,7 +12,7 @@ const AuthScreen = () => {
 	};
 
 	return (
-		<div className='hero-bg relative'>
+		<div className='/hero-bg relative'>
 			{/* Navbar */}
 			<header className='max-w-6xl mx-auto flex items-center justify-between p-4 pb-10'>
 				<img src='/netflix-logo.png' alt='Netflix Logo' className='w-32 md:w-52' />
